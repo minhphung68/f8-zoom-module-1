bb41f6880f0eff95fb52b6357b72811f353bb209
Minh Phung <minh988phung@gmail.com>
2025-07-17 03:02:59 +0700

    bo sung bai tap

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,9 @@ export default defineConfig({
             input: {
                 index: path.resolve(__dirname, "src/index.html"),
                 about: path.resolve(__dirname, "src/about.html"),
+                practice: path.resolve(__dirname, "src/practice.html"),
+                blog: path.resolve(__dirname, "src/blog.html"),
+                contact: path.resolve(__dirname, "src/contact.html")
                 // ...
             },
         },
